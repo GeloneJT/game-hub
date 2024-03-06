9350770ee959bb5599a80d567989391b99d1713f
Jelani <eyeglass0324@gmail.com>
2024-03-06 10:17:52 -0500

    import path correction

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Game } from '../hooks/useGames';
 import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
-import PlatformIconList from './PlatFormIconList';
+import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 
 interface Props {
