42bcccde12427dda6f0c0c7e50ab5c73c1dadf33
Jelani <eyeglass0324@gmail.com>
2024-03-15 10:48:44 -0400

    implement searching

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -4,8 +4,8 @@ const getCroppedImageUrl = (url: string) => {
   if (!url) {
     return noImage;
   }
-  const targert = 'media/';
-  const index = url.indexOf(targert) + targert.length;
+  const target = 'media/';
+  const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 };
 
